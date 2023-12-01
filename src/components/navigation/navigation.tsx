d0a8b3e31118825054059e6ede910c02e805e1b5
Benedikt Kuehn <41262108+benekuehn@users.noreply.github.com>
2023-12-01 21:37:04 +0100

    Challenge 1-1

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -36,7 +36,8 @@ export const Navigation = () => {
         <nav {...props(styles.navigation)}>
             <Link href={"/"} className={stylex(styles.link)}><NavItem label="About this project" isActive={pathname == "/"} /></Link>
             <div className={stylex(styles.divider)}><div>Advent of Code</div></div>
-            <Link href={"/challenges/1"} className={stylex(styles.link)}><NavItem label="Day one" isActive={pathname == "/challenges/1"} /></Link>
+            <Link href={"/challenges/1-1"} className={stylex(styles.link)}><NavItem label="Day one-one" isActive={pathname == "/challenges/1-1"} /></Link>
+            <Link href={"/challenges/1-2"} className={stylex(styles.link)}><NavItem label="Day one-two" isActive={pathname == "/challenges/1-2"} /></Link>
         </nav>
     )
 }
